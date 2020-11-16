f031b9673f1e530a59a32d92dbc5afd31b94851c
hendrick <h.lincertin@gmail.com>
2020-11-16 12:19:30 +0100

    first test succeed

--- contracts/LinkToken.sol
+++ contracts/LinkToken.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
 
-contract LinkToken is ERC20 {
-    constructor() public ERC20("LinkToken", "LNK") {
-        
+contract LinkToken is ERC20, ERC20Burnable, Ownable {
+    constructor() public ERC20("LinkToken", "LNK") {    
     }
 }
